938afc376db848a60f55539c14ec2ac75940658e
Alex Romanov <alexromanov171@gmail.com>
2016-12-13 14:50:19 +0300

    Сменил фразу

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from  telegram.ext import Updater,CommandHandler, MessageHandler, Filters
 
 def greet_user(bot, update):
 	print("Вызван /start")
-	bot.sendMessage(updater.message.chat_id, text="Давай общаться!")
+	bot.sendMessage(update.message.chat_id, text="Как дела!")
 	
 def show_error(bot, update, error):
 	print('Update "{}" caused error "{}"'.format(update, error))
@@ -10,6 +10,12 @@ def show_error(bot, update, error):
 def talk_to_me(bot, update):
 	print("Пришло сообщение: {}".format(update.message.text))
 	bot.sendMessage(update.message.chat_id, update.message.text)
+
+def count_words(bot, update):
+	print(update.message.text.split(' '))
+	bot.sendMessage(update.message.chat_id, text = update.message.text.split)
+	len(update.message.text)
+	
        
 
 def main():
@@ -17,6 +23,7 @@ def main():
 
 	dp = updater.dispatcher
 	dp.add_handler(CommandHandler("start", greet_user))
+	dp.add_handler(CommandHandler("wordcount", count_words))
 	dp.add_handler(MessageHandler([Filters.text], talk_to_me))
 
 	dp.add_error_handler(show_error)
