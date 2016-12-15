13e1828ca24e42de7a2fce06d196a4d93e144de1
Alex Romanov <alexromanov171@gmail.com>
2016-12-15 21:15:59 +0300

    Работающий бот, почти работающий

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from  telegram.ext import Updater,CommandHandler, MessageHandler, Filters
 
 def greet_user(bot, update):
 	print("Вызван /start")
-	bot.sendMessage(update.message.chat_id, text="Как твои дела!")
+	bot.sendMessage(update.message.chat_id, text="Привет, человек! Я бот, который помогает учиться на курсе Learn Python!")
 	
 def show_error(bot, update, error):
 	print('Update "{}" caused error "{}"'.format(update, error))
@@ -16,21 +16,28 @@ def count_words(bot, update):
 	bot.sendMessage(update.message.chat_id, text = update.message.text.split)
 	len(update.message.text)
 	
+di = {"привет": "И тебе привет!", "как дела": "Лучше всех", "пока": "Увидемся"}
+
+def get_answer(k,d):
+	return d[k]
+
+value = get_answer("как дела", di)
+print(value)
+
        
 
-def main():
+def run_bot():
 	updater = Updater("319337485:AAGlmr5E0x_oyJ2-k6Bkxmc12zRUOyU9N0A")
 
 	dp = updater.dispatcher
 	dp.add_handler(CommandHandler("start", greet_user))
 	dp.add_handler(CommandHandler("wordcount", count_words))
 	dp.add_handler(MessageHandler([Filters.text], talk_to_me))
-
 	dp.add_error_handler(show_error)
 
 	updater.start_polling()
 	updater.idle()
 
 if __name__ == '__main__':
-    main()
+    run_bot()
 
